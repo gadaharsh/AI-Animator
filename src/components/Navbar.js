1e6004d03e4faa8dd0a3bb5fc86b262d3b559de4
gadaharsh <gada.hp@somaiya.edu>
2023-05-13 08:18:50 +0530

    Changing to sepolia

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,12 +35,12 @@ function updateButton() {
 async function connectWebsite() {
 
     const chainId = await window.ethereum.request({ method: 'eth_chainId' });
-    if(chainId !== '0x5')
+    if(chainId !== '0x11155111')
     {
-      //alert('Incorrect network! Switch your metamask network to Rinkeby');
+      //alert('Incorrect network! Switch your metamask network to Sepolia');
       await window.ethereum.request({
         method: 'wallet_switchEthereumChain',
-        params: [{ chainId: '0x11155111' }],
+        params: [{ chainId: '0x11155111' }],  
      })
     }  
     await window.ethereum.request({ method: 'eth_requestAccounts' })
